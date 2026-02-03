eadea6c649a96c17d68e1ccfb31f4c52ca7c5aee
JihuN126 <dbwlgns1208@naver.com>
2026-02-03 11:45:20 +0900

    헤더 연동

--- my-react-app/src/components/layout/Header.jsx
+++ my-react-app/src/components/layout/Header.jsx
@@ -1,38 +1,50 @@
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const navigate = useNavigate(); 
+
   const menuItems = [
-    { id: 1, title: '메인 페이지', link: '#' },
-    { id: 2, title: '지도 탐색', link: '#' },
-    { id: 3, title: '커뮤니티', link: '#' },
-    { id: 4, title: '건의사항', link: '#' },
-    { id: 5, title: '포인트샵', link: '#' },
-    { id: 6, title: '마이페이지', link: '#' },
+    { id: 1, title: '메인 페이지', link: '/' },
+    { id: 2, title: '지도 탐색', link: '/map' },
+    { id: 3, title: '커뮤니티', link: '/community' },
+    { id: 4, title: '건의사항', link: '/suggestions' },
+    { id: 5, title: '포인트샵', link: '/shop' },
+    { id: 6, title: '마이페이지', link: '/mypage' },
   ];
 
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <a href="/">EasyEarth</a>
+        <Link to="/">EasyEarth</Link>
       </div>
       
       <nav className={styles.nav}>
         <ul className={styles.menuList}>
           {menuItems.map((item) => (
             <li key={item.id} className={styles.menuItem}>
-              <a href={item.link}>{item.title}</a>
+            
+              <Link to={item.link}>{item.title}</Link>
             </li>
           ))}
         </ul>
       </nav>
 
-      {/* 우측 균형을 맞추기 위한 빈 공간 또는 로그인 버튼 자리 */}
       <div className={styles.auth}>
-        <button className={styles.loginBtn}>Sign in</button>
+        <button 
+          className={styles.loginBtn} 
+          //onClick={}
+        >
+          Sign in
+        </button>
         
-        <button className={styles.loginBtn}>Sign up</button>
+        <button 
+          className={styles.registerBtn}
+          onClick={() => navigate('/signup')}
+        >
+          Sign up
+        </button>
       </div>
-      
     </header>
   );
 };
